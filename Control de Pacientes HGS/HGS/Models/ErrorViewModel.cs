2672f32218614b456250f726d86a23e44b74b62d
0112358132134-2026 <68541@cag.edu.gt>
2023-02-22 10:39:02 -0600

    Project Inicialization
    :D

---
+++ Control de Pacientes HGS/HGS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HGS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
